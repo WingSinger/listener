1e26f2303c2b1d4f98d5ab44b9350fbc7bf13beb
WingSinger <100767460+WingSinger@users.noreply.github.com>
2022-03-31 10:24:15 +0530

    server.py
    A server which creates a terminal to surf the commands on the remote machine

---
+++ server.py
@@ -0,0 +1,39 @@
+from socket import *
+import sys , subprocess
+
+if sys.argv[1:]:
+    HOST = sys.argv[1]
+    PORT = int (sys.argv[2])
+
+HOST = input(".HOST> ")
+PORT = int (input(".PORT> "))
+
+class Server():
+    def __init__(self):
+        self.sock = socket(AF_INET, SOCK_STREAM) # TCP SOCKET
+        self.sock.bind((HOST, PORT))
+        self.sock.listen() 
+        print("[+] Server listening for incoming connections. ")
+        self.clt, self.addr = self.sock.accept()
+        print(f"[+] client ({self.addr}) connected to the server")
+
+    def Terminal(self):
+        prompt = self.clt.recv(1024).decode()
+        while True:
+            cmd = input(prompt).encode()
+            self.clt.send(cmd)
+            size = int (self.clt.recv(1024))
+            opt = self.clt.recv(1024 * size)
+            print(opt.decode())
+
+    def Start():
+        try:
+            Server().Terminal()
+        except EOFError:
+            print("Quitting")
+        except OSError as e:
+            print(f"ERROR : {e}")
+            print("Addres is using somewhere \nUse some other address or kill the process from following command:\nnetstat -t <host:port> ")
+        
+
+Server.Start()
